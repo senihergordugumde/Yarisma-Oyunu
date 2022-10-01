a4d50e580132ea237118bb55635cedad614a785f
emiraksu <emiraksu1608@gmail.com>
2022-10-01 17:14:41 +0300

    Asset değişiklikleri - Getter Methods - Arrays - TextMeshProUGUI

--- Yarisma_Oyunu/Assets/Scripts/CreateQuestion.cs
+++ Yarisma_Oyunu/Assets/Scripts/CreateQuestion.cs
@@ -8,4 +8,23 @@ public class CreateQuestion : ScriptableObject
 {
     [TextArea(2,6)]
     [SerializeField] string question = "Yeni soru";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswerIndex;
+
+
+    public string GetQuestion()
+    {
+        return question;
+    }
+
+    public string GetAnswers(int Index)
+    {
+        return answers[Index];
+    }
+
+    public int getCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
+    }
+
 }
