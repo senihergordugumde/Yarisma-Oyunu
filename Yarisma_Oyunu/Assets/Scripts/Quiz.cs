8458af188aedabff63a9cad30b79f59eea307622
emiraksu <emiraksu1608@gmail.com>
2022-10-02 16:03:02 +0300

    For Loop

--- Yarisma_Oyunu/Assets/Scripts/Quiz.cs
+++ Yarisma_Oyunu/Assets/Scripts/Quiz.cs
@@ -7,9 +7,19 @@ public class Quiz : MonoBehaviour
    
    [SerializeField] TextMeshProUGUI questionText;
    [SerializeField] CreateQuestion question;
+   [SerializeField] GameObject[] answerButtons;
     void Start()
     {
+
         questionText.text = question.GetQuestion();
+
+        
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = question.GetAnswers(i);
+        }
+        
     }
 
 }
